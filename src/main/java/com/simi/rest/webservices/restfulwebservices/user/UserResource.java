7a652d3d831c9c8644d998895b97e1a9ac26ce5a
Simi <nioasca@gmail.com>
2021-04-07 19:45:33 +0300

    Added validation for User.

--- src/main/java/com/simi/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/simi/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -35,7 +36,7 @@ public class UserResource {
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping(value = "/users")
-    public ResponseEntity<Object> createUsers(@RequestBody User user){
+    public ResponseEntity<Object> createUsers(@Valid @RequestBody User user){
         User savedUser = userService.save(user);
 
         URI location = ServletUriComponentsBuilder
