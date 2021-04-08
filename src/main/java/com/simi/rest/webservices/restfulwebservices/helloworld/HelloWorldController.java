c06574d389221afff03913ff7c4234aad908bd87
Simi <nioasca@gmail.com>
2021-04-08 20:00:48 +0300

    Added i18n for HelloWorldController.

--- src/main/java/com/simi/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/simi/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.simi.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(value = "/hello-world")
     public String helloWorld(){
@@ -21,6 +31,11 @@ public class HelloWorldController {
     @GetMapping(value = "/path-variable/{name}", produces = MediaType.APPLICATION_XML_VALUE)
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
+    @GetMapping(value = "/hello-world-i18n")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return  messageSource.getMessage("good.morning.message", null, locale);
     }
 
 }
