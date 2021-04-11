f4e0240b4b44801a49289fff75381bc4ca0d82c7
Simi <nioasca@gmail.com>
2021-04-11 12:07:53 +0300

    Added Post entity.

--- src/main/java/com/simi/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/simi/rest/webservices/restfulwebservices/user/User.java
@@ -6,9 +6,11 @@ import io.swagger.annotations.ApiModelProperty;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @ApiModel(description = "All detaild bout the user")
 @Entity
@@ -26,6 +28,9 @@ public class User {
     @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
 
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
     public User() {
     }
 
@@ -33,6 +38,14 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     public Integer getId() {
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,3 +1,5 @@
 insert into User values (1001, sysdate(), 'Ana');
 insert into User values (1002, sysdate(), 'Maria');
 insert into User values (1003, sysdate(), 'Simi');
+insert into Post values (1101, 'My first post', 1001);
+insert into Post values (1102, 'My second post', 1001);
